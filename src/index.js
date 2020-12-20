80b09feea1cba3a1dee8151d1693ac0214bc2cd9
emiller7231 <34141292+emiller7231@users.noreply.github.com>
2020-12-20 12:04:53 -0600

    Add bumla and full sentences

--- src/index.js
+++ src/index.js
@@ -10,26 +10,26 @@ function Letter(props) {
 
 class Word extends React.Component {
     isRigthOrWrong(letter, index) {
-        return letter === this.props.word[index] ? "right" : "wrong";
+        return letter === this.props.current[index] ? "right" : "wrong";
     }
 
     render() {
         const inputLetters = Array.from(this.props.input)
             .map((letter, index) => {
                 return (
-                    <Letter key={this.index} status={this.isRigthOrWrong(letter, index)} value = {letter} />
+                    <Letter key={index} status={this.isRigthOrWrong(letter, index)} value = {letter} />
                 );
             });
-        const letters = Array.from(this.props.word)
+        const letters = Array.from(this.props.current)
             .slice(this.props.input.length)
             .map((letter, index) => {
                 return (
-                    <Letter key={this.index} status="nothing" value = {letter} />
+                    <Letter key={index} status="nothing" value = {letter} />
                 );
             });
 
         return (
-            <div>
+            <div className="title">
                 {inputLetters}
                 {letters}
             </div>
@@ -41,28 +41,65 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            word: "Testing",
+            isLoaded: false,
+            options: [],
+            error: "",
+            current: "",
             input: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
     }
 
+    componentDidMount() {
+        fetch("/sentences.txt")
+            .then(res => res.text())
+            .then(
+                (result) => {
+                    let optionsArray = result.split(`\r\n`);
+                    this.setState({
+                        isLoaded: true,
+                        options: optionsArray,
+                        current: optionsArray[getRandomInt(0, optionsArray.length)]
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+    }
+
     handleChange(event) {
-        console.log(event.target.value);
-        if (event.target.value.length <= this.state.word.length) {
+        if (event.target.value.length <= this.state.current.length) {
             this.setState({
                 input: event.target.value
             });
+            if (this.state.current === event.target.value) {
+                this.setState({
+                    current: this.state.options[getRandomInt(0, this.state.options.length)],
+                    input: ""
+                });
+            }
         }
     }
 
     render() {
         return (
-            <div>
-                <Word word={this.state.word} input={this.state.input}/>
-                <label>Type the word above:</label><br />
-                <input type="text" value={this.state.input} onChange={this.handleChange}/>
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-three-fifths is-offset-one-fifth is-centered">
+                        <Word current={this.state.current} input={this.state.input}/>
+                        <div className="field">
+                            <label className="label">Type the sentence above:</label>
+                            <div className="control">
+                                <input type="text" className="input" value={this.state.input} onChange={this.handleChange}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -72,3 +109,9 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
